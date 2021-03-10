adf1f54cb98208ce755582c7f1897b398d4dd0c3
danBamikiya <dudeawesome732@gmail.com>
2021-03-10 12:24:24 +0100

    feat: supports dotenv :hammer:

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
-const path = require("path");
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-	entry: "./src/js/index.js", //location of your js files
+	mode: 'development',
+	entry: './src/js/index.js', //location of your js files
 	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "js/main.js" // where js files would be bundled to
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/main.js' // where js files would be bundled to
 	},
 	watch: true,
 	module: {
@@ -13,9 +15,10 @@ module.exports = {
 				test: /\.js$/, //using regex to tell babel exactly what files to transcompile
 				exclude: /node_modules/, // files to be ignored
 				use: {
-					loader: "babel-loader" // specify the loader
+					loader: 'babel-loader' // specify the loader
 				}
 			}
 		]
-	}
+	},
+	plugins: [new Dotenv()]
 };
