f07a3864b8a42caeee1acc786894e6be413a9449
danBamikiya <dudeawesome732@gmail.com>
2021-05-06 01:10:52 +0100

    refactor: set actor img url and parsed actor name in cast property of Movie :recycle:

--- src/js/getActorsImgsUrl.js
+++ src/js/getActorsImgsUrl.js
@@ -1,27 +1,18 @@
 import actorNameParser from './actorNameParser';
 import scrapeWebForActorsImages from './webScrapper';
 
-function getActorsImgsURL(Movie) {
-	const actorsImgsURL = {};
-	Promise.all(
-		Movie.parsedActorNames.map(async (actorName, index) => {
-			actorsImgsURL[
-				Movie.cast[index].actor
-			] = await scrapeWebForActorsImages(actorName);
-		})
-	).then((Movie.actorsImgsURL = actorsImgsURL));
+function getActorsImgsURL(movieCast) {
+	movieCast.forEach(
+		async actor =>
+			(actor['actorImgURL'] = await scrapeWebForActorsImages(
+				actor['parsedActorName']
+			))
+	);
 }
 
-export default function backgroundActorNamesParse(Movie) {
-	const parsedActorNames = Movie.cast
-		.map(actor => actor['actor'])
-		.reduce(
-			(allActorNames, actorName) => [
-				...allActorNames,
-				actorNameParser(actorName)
-			],
-			[]
-		);
-	Movie.parsedActorNames = parsedActorNames;
-	getActorsImgsURL(Movie);
+export default function backgroundActorNamesParse({ cast: movieCast }) {
+	movieCast.forEach(
+		actor => (actor['parsedActorName'] = actorNameParser(actor['actor']))
+	);
+	getActorsImgsURL(movieCast);
 }
