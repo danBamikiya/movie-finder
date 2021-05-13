a11dcc5a2d07197c5e0b8cbc47d7b24d243387f0
danBamikiya <dudeawesome732@gmail.com>
2021-05-13 10:07:54 +0100

    refactor: modify the actor name parser to parse other input types :recycle:

--- src/js/actorNameParser.js
+++ src/js/actorNameParser.js
@@ -1,4 +1,9 @@
-export default function actorNameParser(actorName) {
+/*
+	@params	actorName: string 	eg: 'Tony Stark'
+	@desc	This convert uppercased names like Tony Stark to IMDB query-able names like tony+stark
+*/
+
+function parseFromCasedName(actorName) {
 	/** Regex to parse actors name */
 	const regexParser = /([^\W])([\w]+)/gi;
 	const result = actorName.match(regexParser);
@@ -9,4 +14,11 @@ export default function actorNameParser(actorName) {
 	const name = parsedName.slice(0, -1); // remove ending '+' sign
 	const casedName = name.toLowerCase();
 	return casedName;
+}
+
+export default function actorNameParser(actorName, type) {
+	if (type === 'casedName') return parseFromCasedName(actorName);
+	if (type === 'urlWithName') return parseFromUrl(actorName);
+
+	return actorName;
 }
