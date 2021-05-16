51953bda4f715777bacec515edb70294d332f000
danBamikiya <dudeawesome732@gmail.com>
2021-05-16 22:59:23 +0100

    chore(webpack): use sourcemaps only in development :hammer:

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
-const { SourceMapDevToolPlugin } = require('webpack');
 
 module.exports = {
 	mode: 'development',
-	entry: ['./src/js/index.js', './src/js/useHoverCard.js'], //location of js files
+	entry: ['./src/js/index.js', './src/js/useHoverCard.js'], //entrypoint of js files
 	output: {
 		path: path.resolve(__dirname, 'public'),
 		filename: 'js/main.js' // where js files would be bundled to
@@ -21,11 +20,11 @@ module.exports = {
 			}
 		]
 	},
-	devtool: false, // comment this out to view source files in the browser DevTools
+	devtool: process.env.NODE_ENV === 'development', // enable source maps in only in development
 	resolve: {
 		fallback: {
 			fs: false
 		}
 	},
-	plugins: [new Dotenv(), new SourceMapDevToolPlugin({})] // comment out 'new SourceMapDevToolPlugin({})' to view source files in the browser DevTools
+	plugins: [new Dotenv()]
 };
