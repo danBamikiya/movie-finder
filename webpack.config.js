915129f276db158ac50a2e19e4b2f905a93fed0d
danBamikiya <dudeawesome732@gmail.com>
2021-05-29 16:16:06 +0100

    fix: remove unused entrypoint file :adhesive_bandage:

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const outputDir = 'public';
 
 module.exports = {
 	mode: __DEV__ ? 'development' : 'production',
-	entry: ['./src/js/index.ts', './src/js/hooks/useHoverCard.ts'], //entrypoint of js files
+	entry: ['./src/js/index.ts'], //entrypoint of js files
 	output: {
 		path: path.resolve(__dirname, outputDir),
 		filename: 'js/main.js' // where js files will be bundled to
