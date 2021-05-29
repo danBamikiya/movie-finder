3ee117a6d4f07f23860c22ceb5ff00d2d98900d0
danBamikiya <dudeawesome732@gmail.com>
2021-05-29 03:08:07 +0100

    fix: querySelector bug in movie rendering fixed :bug:

--- src/js/ui/Movie/index.ts
+++ src/js/ui/Movie/index.ts
@@ -8,8 +8,8 @@ export default function renderMovie(movie: Movie) {
 	const container = document.querySelector('.container');
 	if (!(container instanceof HTMLElement)) return;
 
-	document.querySelector('movie-poster-details-container')?.remove();
-	document.querySelector('movie-poster-container')?.remove();
+	document.querySelector('.movie-poster-details-container')?.remove();
+	document.querySelector('.movie-poster-container')?.remove();
 
 	const fragment = cachedMovieDocumentFragment(movie);
 
