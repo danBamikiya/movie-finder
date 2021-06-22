1e03de23f33440d2d4ec684dada55c5a9f0c3802
danBamikiya <dudeawesome732@gmail.com>
2021-06-22 10:20:49 +0100

    refactor: cache actor img by id :recycle:

--- src/ts/actions/scrapeImage.ts
+++ src/ts/actions/scrapeImage.ts
@@ -1,9 +1,6 @@
 // TODO:	Find a lighter dom parser. Cheerio is too heavy
 import { load } from 'cheerio';
-import { memoize } from '../lib';
 import { scrapeWeb } from '../utils';
-
-const cachedFetchSafeResponse = memoize(scrapeWeb);
 
 function parseResponse(response: { contents: string }): string | undefined {
 	const html = response.contents;
@@ -13,7 +10,6 @@ function parseResponse(response: { contents: string }): string | undefined {
 	//selecting the element with the actor's image url
 	const scrapedata = $('#name-poster').attr('src');
 
-	//payoff scraped data
 	return scrapedata;
 }
 
@@ -23,17 +19,5 @@ export async function scrapeActorImgUrl(
 	if (!actorId.length) return '';
 
 	const url = `https://www.imdb.com/name/${actorId}/`;
-
-	/**
-	 * Fetch the imdb page of the actor using a webScrapper utility function.
-	 * Callbacks to parse the response are also passed to the function.
-	 * The scraped image url is cached.
-	 *
-	 * NOTE: This level of caching caches the individual scraped images so
-	 * that when a new searched movie has same actors as other previously searched
-	 * movies those cached actors images is returned rather than scraping for the
-	 * new movie's actors images.
-	 */
-
-	return await cachedFetchSafeResponse(url, parseResponse);
+	return await scrapeWeb(url, parseResponse);
 }
--- src/ts/actions/setActorsImgsUrl.ts
+++ src/ts/actions/setActorsImgsUrl.ts
@@ -1,9 +1,13 @@
+import { memoize } from '../lib';
 import { Actor, Movie } from '../types';
 import { scrapeActorImgUrl } from './scrapeImage';
 import { useActorImgUrl } from '../hooks/useActorImgUrl';
 
+const cachedFetchSafeResponse = memoize(scrapeActorImgUrl);
+
 async function fetchActorImgUrl(actor: Actor, index: number) {
-	actor['actorImgURL'] = await scrapeActorImgUrl(actor['actor_id']);
+	// Cache the individual scraped images
+	actor['actorImgURL'] = await cachedFetchSafeResponse(actor['actor_id']);
 
 	useActorImgUrl(actor.actorImgURL, index);
 }
